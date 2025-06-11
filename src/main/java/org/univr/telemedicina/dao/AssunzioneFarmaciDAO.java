7385bb55736cfe5deeeeea996d9fe604e7617d82
Riccardo Morelli <riccardomorelli2003@gmail.com>
2025-06-11 16:40:29 +0200

    Merge pull request #3
    * Added AssunzioneFarmaci.java AssunzioniFarmaciDAO.java, half done.
    
    * Modified TimestampAssunzione from type Timestamp to LocalDateTime.
    
    * Completed AssunzioniFarmaciDAO.java, added constructor AssunzioneFarm…
    
    * Renamed AssunzioniFarmaciDAO.java in AssunzioneFarmaciDAO.java

---
+++ src/main/java/org/univr/telemedicina/dao/AssunzioneFarmaciDAO.java
@@ -0,0 +1,125 @@
+package org.univr.telemedicina.dao;
+
+import org.univr.telemedicina.model.AssunzioneFarmaci;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+/**
+ * Classe DAO per gestire le operazioni di accesso ai dati relativi alle assunzioni di farmaci.
+ * Deve aggiungere una assunzione da parte di una paziente, e legge tutte assunzioni di farmaci per paziente,
+ * e assunzioni di un determinato giorno.
+ */
+public class AssunzioneFarmaciDAO {
+
+    /**
+     * Legge tute le assunzioni di farmaci per un paziente specifico.
+     * @param IDPaziente L'ID del paziente
+     * @return Una List contenente le AssunzioniFarmaci per il paziente specificato.
+     */
+    public List<AssunzioneFarmaci> leggiAssunzioniFarmaci(int IDPaziente){
+
+        // Lista per memorizzare le assunzioni di farmaci
+        List<AssunzioneFarmaci> assunzioni = new ArrayList<>();
+
+        String sql = "SELECT * FROM AssunzioniFarmaci WHERE IDPaziente = ?";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+            //imposto il parametro ? per evitare sql injection
+            pstmt.setInt(1, IDPaziente);
+
+            try(ResultSet rs = pstmt.executeQuery()) {
+
+                while(rs.next()){
+                    AssunzioneFarmaci as = new AssunzioneFarmaci(
+                            rs.getInt("IDAssunzione"),
+                            rs.getInt("IDTerapia"),
+                            rs.getInt("IDPaziente"),
+                            rs.getObject("TimestampAssunzione", LocalDateTime.class),
+                            rs.getString("QuantitaAssunta")
+                    );
+                    assunzioni.add(as);
+                }
+            }
+        } catch(SQLException e){
+            System.err.println("Errore durante la lettura dei farmaci assunti: " + e.getMessage());
+        }
+        // Se non viene trovata nessuna assunzione o si verifica un errore, ritorna una lista vuota
+        return assunzioni;
+    }
+
+     /**
+      * Legge le assunzioni di farmaci in un giorno per un paziente specifico.
+      * @param IDPaziente L'ID del paziente
+      * @param data La data per cui si vogliono leggere le assunzioni
+      * @return Una List contenente le AssunzioniFarmaci per il paziente specificato.
+      */
+     public List<AssunzioneFarmaci> leggiAssunzioniGiorno(int IDPaziente, LocalDate data){
+         List<AssunzioneFarmaci> assunzioni = new ArrayList<>();
+
+         // Query SQL per leggere le assunzioni di farmaci in un giorno specifico
+         String sql =   "SELECT * FROM AssunzioniFarmaci WHERE IDPaziente = ? " +
+                        "AND TimestampAssunzione >= ?" +
+                        " AND TimestampAssunzione < ?";
+
+         // Definisce l'intervallo di tempo per l'intera giornata
+         LocalDateTime inizioGiorno = data.atStartOfDay(); // Es. 2025-06-11T00:00:00
+         LocalDateTime inizioGiornoSuccessivo = data.plusDays(1).atStartOfDay(); // Es. 2025-06-12T00:00:00
+
+         try (Connection conn = DatabaseManager.getConnection();
+              PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+             //imposto il parametro ? per evitare sql injection
+             pstmt.setInt(1, IDPaziente);
+             pstmt.setObject(2, inizioGiorno);
+                pstmt.setObject(3, inizioGiornoSuccessivo);
+
+             try(ResultSet rs = pstmt.executeQuery()) {
+
+                 while(rs.next()){
+                     AssunzioneFarmaci as = new AssunzioneFarmaci(
+                             rs.getInt("IDAssunzione"),
+                             rs.getInt("IDTerapia"),
+                             rs.getInt("IDPaziente"),
+                             rs.getObject("TimestampAssunzione", LocalDateTime.class),
+                             rs.getString("QuantitaAssunta")
+                     );
+                     assunzioni.add(as);
+                 }
+             }
+         } catch(SQLException e){
+             System.err.println("Errore durante la lettura dei farmaci assunti: " + e.getMessage());
+         }
+         // Se non viene trovata nessuna assunzione o si verifica un errore, ritorna un Optional vuoto
+         return assunzioni;
+     }
+
+    /**
+     * Aggiunge una nuova assunzione di farmaci per un paziente.
+     * @param assunzione L'oggetto AssunzioneFarmaci da aggiungere
+     */
+    public void aggiungiAssunzione(AssunzioneFarmaci assunzione) {
+        String sql = "INSERT INTO AssunzioniFarmaci(IDTerapia, IDPaziente, TimestampAssunzione, QuantitaAssunta) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // Imposta i parametri della query
+            pstmt.setInt(1, assunzione.getIDTerapia());
+            pstmt.setInt(2, assunzione.getIDPaziente());
+            pstmt.setObject(3, assunzione.getTimestampAssunzione());
+            pstmt.setString(4, assunzione.getQuantitaAssunta());
+
+            // Esegue l'inserimento
+            pstmt.executeUpdate();
+
+        } catch (SQLException e){
+            System.err.println("Errore durante l'aggiunta di assunzione di farmaci: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/org/univr/telemedicina/model/AssunzioneFarmaci.java
@@ -0,0 +1,66 @@
+package org.univr.telemedicina.model;
+
+import java.time.LocalDateTime;
+
+public class AssunzioneFarmaci {
+    private int IDAssunzione;
+    private int IDTerapia;
+    private int IDPaziente;
+    private LocalDateTime TimestampAssunzione;
+    private String QuantitaAssunta;
+
+
+    //costruttore per quando si legge
+    public AssunzioneFarmaci(int IDAssunzione, int IDTerapia, int IDPaziente, LocalDateTime TimestampAssunzione, String QuantitaAssunta) {
+        this.IDAssunzione = IDAssunzione;
+        this.IDTerapia = IDTerapia;
+        this.IDPaziente = IDPaziente;
+        this.TimestampAssunzione = TimestampAssunzione;
+        this.QuantitaAssunta = QuantitaAssunta;
+    }
+
+    //costruttore per quando si inserisce, non c'è IDAssunzione perché è auto-incrementato nel database
+    public AssunzioneFarmaci(int IDTerapia, int IDPaziente, LocalDateTime TimestampAssunzione, String QuantitaAssunta) {
+        this.IDTerapia = IDTerapia;
+        this.IDPaziente = IDPaziente;
+        this.TimestampAssunzione = TimestampAssunzione;
+        this.QuantitaAssunta = QuantitaAssunta;
+    }
+
+    //metodi getter e setter
+    public int getIDAssunzione() {
+        return IDAssunzione;
+    }
+    public void setIDAssunzione(int IDAssunzione) {
+        this.IDAssunzione = IDAssunzione;
+    }
+
+    public int getIDTerapia() {
+        return IDTerapia;
+    }
+    public void setIDTerapia(int IDTerapia) {
+        this.IDTerapia = IDTerapia;
+    }
+
+    public int getIDPaziente() {
+        return IDPaziente;
+    }
+    public void setIDPaziente(int IDPaziente) {
+        this.IDPaziente = IDPaziente;
+    }
+
+    public LocalDateTime getTimestampAssunzione() {
+        return TimestampAssunzione;
+    }
+    public void setTimestampAssunzione(LocalDateTime TimestampAssunzione) {
+        this.TimestampAssunzione = TimestampAssunzione;
+    }
+
+    public String getQuantitaAssunta() {
+        return QuantitaAssunta;
+    }
+    public void setQuantitaAssunta(String QuantitaAssunta) {
+        this.QuantitaAssunta = QuantitaAssunta;
+    }
+
+}
