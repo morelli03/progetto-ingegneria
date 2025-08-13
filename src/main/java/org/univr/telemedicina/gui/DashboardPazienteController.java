05c969a224021c58f8d12c20fa77bad5cef2fbf6
riccardo <riccardomorelli2003@gmail.com>
2025-08-13 17:24:58 +0200

    Add patient dashboard functionality: implement UI components and controller for patient interactions

---
+++ src/main/java/org/univr/telemedicina/gui/DashboardPazienteController.java
@@ -0,0 +1,52 @@
+package org.univr.telemedicina.gui;
+
+import javafx.event.ActionEvent;
+import org.univr.telemedicina.model.Utente;
+
+public class DashboardPazienteController {
+
+    private Utente pazienteLoggato;
+
+    public void initData(Utente paziente){
+        this.pazienteLoggato = paziente;
+    }
+
+    public void handleNotificationClick(ActionEvent actionEvent) {
+    }
+
+    public void handleLogout(ActionEvent actionEvent) {
+    }
+
+    public void handleLeftProfiloButton(ActionEvent actionEvent) {
+    }
+
+    public void handleRightProfiloButton(ActionEvent actionEvent) {
+    }
+
+    public void handleLeftTerapieButton(ActionEvent actionEvent) {
+    }
+
+    public void handleRightTerapieButton(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveButtonMisurazioni(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveButtonAssunzioneFarmaci(ActionEvent actionEvent) {
+    }
+
+    public void handleAggModSintomoButton(ActionEvent actionEvent) {
+    }
+
+    public void handleAggModPatologiaButton(ActionEvent actionEvent) {
+    }
+
+    public void handleAggModTerapiaConcomButton(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteButtonAzioni(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveButtonAzioni(ActionEvent actionEvent) {
+    }
+}
